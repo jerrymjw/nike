2a6905e4af264072a0cf488ac655f8aea98660b5
Jerry Ma <leilingjerry@gmail.com>
2023-09-20 09:46:11 +0930

    this for nike showcase project

---
+++ src/components/cards/index.js
@@ -0,0 +1,11 @@
+import ShoeCard from './ShoeCard';
+import PopularProductCard from './PopularProductCard';
+import ServiceCard from './ServiceCard';
+import ReviewCard from './ReviewCard';
+
+export {
+  ShoeCard,
+  PopularProductCard,
+  ServiceCard,
+  ReviewCard,
+}
---
+++ src/components/index.js
@@ -0,0 +1,16 @@
+import Button from "./Button";
+import Nav from "./Nav";
+import {
+    ShoeCard,
+    PopularProductCard,
+    ServiceCard,
+    ReviewCard,
+} from "./cards";
+export {
+    Button,
+    Nav,
+    ShoeCard,
+    PopularProductCard,
+    ServiceCard,
+    ReviewCard,
+}
